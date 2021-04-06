ecf37a342c5e8f1620ccdecbf994238a9e35eab3
programacion1umg@outlook.com <programacion1umg@outlook.com>
2021-04-05 20:40:45 -0600

    modificiaciones a la clase usuario

--- DisneyPlus/CODIGO/usuario.cs
+++ DisneyPlus/CODIGO/usuario.cs
@@ -8,5 +8,20 @@ namespace DisneyPlus.CODIGO
 {
     class usuario
     {
+        int usuarioID { get; set; }
+        string nombre { get; set; }
+        string correo { get; set; }
+        
+        public usuario()
+        {
+
+        }
+
+        public usuario(int codigo, string name, string email)
+        {
+            usuarioID = codigo;
+            nombre = name;
+            correo = email;
+        }
     }
 }
